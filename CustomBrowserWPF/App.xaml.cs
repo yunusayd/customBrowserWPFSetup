38a678a7f677dae0172747723b16805449bf981e
yunus.aydogmus <yunus.aydogmus@softtech.com.tr>
2019-12-26 13:17:56 +0300

    initialcommit

---
+++ CustomBrowserWPF/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.IO;
+using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            if (ConfigurationManager.AppSettings.AllKeys.Contains("CefSettings.RemoteDebuggingPort"))
+            {
+                settings.RemoteDebuggingPort = Convert.ToInt32(ConfigurationManager.AppSettings["CefSettings.RemoteDebuggingPort"]);
+            }            
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+        }
+    }
+}
---
+++ CustomBrowserWPF/TabItem.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Configuration;
+using System.Windows;
+using System.Linq;
+using System;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class TabItem : Window
+    {
+
+        public bool WebMode
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings.AllKeys.Contains("WebMode") ?
+                    Convert.ToBoolean(ConfigurationManager.AppSettings["WebMode"]) : false;
+            }
+        }
+        public String MainPage
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings.AllKeys.Contains("mainPage") ?
+                    ConfigurationManager.AppSettings["mainPage"] : "http://dyp.isyatirim.com.tr";
+            }
+        }
+
+        public TabItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.Stop();
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void frmMain_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.F5)
+            {
+                Browser.Reload();
+            }
+            else if (e.Key == System.Windows.Input.Key.F11)
+            {
+
+            }
+        }
+    }
+}
